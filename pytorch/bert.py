7b3d39c736fcea574ecedd83f3e39fc926b32e50
allen_kuo <allen_kuo@trend.com.tw>
2018-12-27 09:36:26 +0000

    add BERT+GRU

--- pytorch/bert.py
+++ pytorch/bert.py
@@ -38,8 +38,8 @@ from IPython.core.debugger import set_trace
 # In[6]:
 
 
-from time import time
-
+from IPython import display
+import matplotlib.pyplot as plt
 
 # In[7]:
 
@@ -53,13 +53,17 @@ learning_rate = 1e-3
 
 
 # In[8]:
+learning_rate = 1e-5
+train_fpath = '../data/quora/train'
+val_fpath = '../data/quora/val'
 
 
 class QuoraDataset(Dataset):
-    def __init__(self, fpath, transform=None):
+    def __init__(self, fpath, transform=None, frac=1.0):
         self.fpath = fpath
-        self.df = pd.read_csv(fpath).sample(frac=1.0)
+        self.df = pd.read_csv(fpath, error_bad_lines=False).sample(frac=frac)
         self.df = self.df.dropna()
+        self.df['is_duplicate'] = self.df['is_duplicate'].astype(int)
         self.transform = transform
         
     def __len__(self):
@@ -69,7 +73,11 @@ class QuoraDataset(Dataset):
         item = self.df.iloc[index]
         sample = [item['is_duplicate'], item['question1'], item['question2']]
         if self.transform:
-            sample = self.transform(sample)
+            try:
+                sample = self.transform(sample)
+            except:
+                print(sample)
+                raise
         return sample        
 
 
@@ -196,50 +204,132 @@ class BertSentenceDifference(torch.nn.Module):
             #set_trace()
             raise
 
+class BertGRU(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert_model = BertModel.from_pretrained('bert-base-uncased')
+        for p in self.bert_model.parameters():
+            p.requires_grad = False
+            
+        #self.rnn = torch.nn.GRU(input_size=768, hidden_size=768, num_layers=1, batch_first=True, dropout=0, bidirectional=False)
+        self.rnn = torch.nn.GRU(input_size=768, hidden_size=768, num_layers=2, batch_first=True, dropout=0.8, bidirectional=False)
+        
+        #self.initial_rnn_hidden = torch.rand(1, 1, 768)
+        self.initial_rnn_hidden = torch.rand(2, 1, 768)
+        
+        self.initial_rnn_hidden = torch.cat([self.initial_rnn_hidden]*batch_size, dim=1)
+        
+        #self.fc = torch.nn.Linear(in_features=768*2, out_features=2)
+        self.fc = torch.nn.Linear(in_features=768*2*2, out_features=2)
+        
+        self.fc_act = torch.nn.Softmax()
+        
+    def forward(self, X):
+        try:
+            sent1, sent2 = X[:, :sentence_length], X[:, sentence_length:]
+            hiddens = []
+            for sent in [sent1, sent2]:
+                segment_tensor = torch.zeros_like(sent)
+                encoder, _ = self.bert_model(sent, segment_tensor)
+            
+                output, last_hidden = self.rnn(encoder[-1], self.initial_rnn_hidden[:, :X.shape[0], :])
+                #hiddens.append(last_hidden.view(X.shape[0], 768))
+                hiddens.append(last_hidden.view(X.shape[0], 768*2))
+            
+            diff = torch.cat(hiddens, dim=1)
+            
+            logits = self.fc(diff)
+            logits = self.fc_act(logits)
+            return logits
+        
+        except:
+            #set_trace()
+            raise
 
+class Metrics:
+    def __init__(self):
+        self.train_metrics = pd.DataFrame(columns=['loss', 'accuracy'])
+        self.val_metrics = pd.DataFrame(columns=['loss', 'accuracy'])
+        
+    def append_row(self, df, row):
+        row = pd.DataFrame([row], columns=row.keys())
+        return pd.concat([df, row], axis=0)
+        
+    def add_train(self, metric):
+        self.train_metrics = self.append_row(self.train_metrics, metric)
+        
+    def add_val(self, metric):
+        self.val_metrics = self.append_row(self.val_metrics, metric)
+
+    def is_best_val(self):
+        return self.val_metrics['accuracy'].max() == self.val_metrics['accuracy'].iloc[-1]
+
+def build_dataloader(fpath, frac=1.0):
+    qdata = QuoraDataset(fpath, frac=frac)
+    transform = transforms.Compose([
+        TokToID(),
+        FixSentencesLength(sentence_length),
+        Concat(),
+        ToTensor()
+    ])
+    qdata.transform = transform
+    dataloader = DataLoader(
+        dataset=qdata,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers
+    )
+    
+    return dataloader
+
+
+def print_named_parameter(model):
+    for p in model.named_parameters():
+        print(p[0])
+
+def plot_metrics(metrics):
+    plt.gcf().clear()
+    plt.plot(metrics.train_metrics['Accuracy'].values, color='r')
+    plt.plot(metrics.train_metrics['Loss'].values, color='g')
+    plt.plot(metrics.val_metrics['Loss'].values, color='b')
+    plt.plot(metrics.val_metrics['Accuracy'].values, color='y')
+    display.display(plt.gcf())
+    display.clear_output(wait=True)
 # In[11]:
 
 
-qdata = QuoraDataset(fpath)
-transform = transforms.Compose([
-    TokToID(),
-    FixSentencesLength(sentence_length),
-    Concat(),
-    ToTensor()
-])
-qdata.transform = transform
-dataloader = DataLoader(
-    dataset=qdata,
-    batch_size=batch_size,
-    shuffle=shuffle,
-    num_workers=num_workers
-)
+if __name__ == '__main__':
+    train_dl = build_dataloader(train_fpath, 0.001)
+    val_dl = build_dataloader(val_fpath, 0.0001)
 
+    from torch.nn import CrossEntropyLoss
+    from torch.optim import Adam, SGD
+    model = BertGRU()
+    loss = CrossEntropyLoss()
+    trainable_tensors = [p[1] for p in model.named_parameters() if p[0].startswith('rnn') or p[0].startswith('fc')]
+    optimizer = Adam(params=trainable_tensors, lr=learning_rate)
 
-# In[ ]:
+    from ignite.engine import create_supervised_trainer, Events, create_supervised_evaluator
+    from ignite.metrics import Loss, Accuracy
+    trainer = create_supervised_trainer(model=model, optimizer=optimizer, loss_fn=loss)
+    evaluator = create_supervised_evaluator(model=model, metrics={'Loss': Loss(loss), 'Accuracy': Accuracy()})
+    metrics = Metrics()
 
-
-from torch.nn import CrossEntropyLoss
-from torch.optim import Adam, SGD
-model = BertSentenceDifference()
-loss = CrossEntropyLoss()
-trainable_tensors = [p[1] for p in model.named_parameters() if p[0].startswith('fc2')]
-optimizer = Adam(params=trainable_tensors, lr=learning_rate)
-
-from ignite.engine import create_supervised_trainer, Events, create_supervised_evaluator
-from ignite.metrics import Loss, Precision
-trainer = create_supervised_trainer(model=model, optimizer=optimizer, loss_fn=loss)
-evaluator = create_supervised_evaluator(model=model, metrics={'Loss': Loss(loss), 'precision': Precision()})
-
-epoch_st = time()
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_training_loss(trainer):
-    evaluator.run(dataloader)
-    global epoch_st
-    elasped_time = int(time()-epoch_st)
     epoch_st = time()
-    print(f"epoch {trainer.state.epoch} {evaluator.state.metrics} {elasped_time}")
-    
-trainer.run(dataloader, max_epochs=100)
-
-
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_training_loss(trainer):
+        evaluator.run(train_dl)
+        metrics.add_train(evaluator.state.metrics)
+        
+        global epoch_st
+        elasped_time = int(time()-epoch_st)
+        epoch_st = time()
+        
+        print(f"epoch {trainer.state.epoch} {evaluator.state.metrics} {elasped_time}")
+        
+        evaluator.run(val_dl)
+        metrics.add_val(evaluator.state.metrics)
+        
+        plot_metrics(metrics)
+        
+    trainer.run(train_dl, max_epochs=100)
